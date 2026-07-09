190f1ce16bac5ee043ec4be0fcb43140e49cad52
ArtemQa <82034384+ArtemQa8@users.noreply.github.com>
2026-07-09 22:48:07 +0300

    ОНО ПОШЛО!!!

--- Homework06/internal/model/rules.go
+++ Homework06/internal/model/rules.go
@@ -19,3 +19,22 @@ func abs(x int) int {
 	}
 	return x
 }
+
+func ПравилаДля(фигура *Фигура) ФигураСПравилами {
+	switch фигура.Тип() {
+	case Пешка:
+		return &Pawn{Цвет: фигура.Цвет()}
+	case Ладья:
+		return &Rook{Цвет: фигура.Цвет()}
+	case Конь:
+		return &Knight{Цвет: фигура.Цвет()}
+	case Слон:
+		return &Bishop{Цвет: фигура.Цвет()}
+	case Ферзь:
+		return &Queen{Цвет: фигура.Цвет()}
+	case Король:
+		return &King{Цвет: фигура.Цвет()}
+	default:
+		return nil
+	}
+}
