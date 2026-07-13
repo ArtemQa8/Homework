48abb053b67ce9760ea2542c5ba93d98e2467d6e
ArtemQa <82034384+ArtemQa8@users.noreply.github.com>
2026-07-13 22:53:24 +0300

    Кажется, оно работает

--- Homework06/internal/model/rules.go
+++ Homework06/internal/model/rules.go
@@ -4,22 +4,6 @@ type ФигураСПравилами interface {
 	МожетХодить(ход *Ход, доска *Доска) bool
 }
 
-func знак(x int) int {
-	if x > 0 {
-		return 1
-	} else if x < 0 {
-		return -1
-	}
-	return 0
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
 func ПравилаДля(фигура *Фигура) ФигураСПравилами {
 	switch фигура.Тип() {
 	case Пешка:
